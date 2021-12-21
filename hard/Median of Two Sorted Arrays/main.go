2293cbe00c6e1dcb1ca19b04b6e18ea49fd8bec1
Alexander Nurullov <web_soft@mail.ru>
2021-12-21 20:38:36 +0400

    add hard task

---
+++ hard/Median of Two Sorted Arrays/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+*/
+
+func main() {
+	fmt.Println(findMedianSortedArrays([]int{1, 3}, []int{2}))
+	fmt.Println(findMedianSortedArrays([]int{1, 2}, []int{3, 4}))
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	arr := append(nums1, nums2...)
+	sort.Ints(arr)
+
+	mNumber := len(arr) / 2
+
+	if len(arr)%2 != 0 {
+		return float64(arr[mNumber])
+	}
+
+	return float64(arr[mNumber-1]+arr[mNumber]) / 2
+}
