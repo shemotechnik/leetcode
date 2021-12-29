81f82f55f8b653d288adfb8c5914a7307e21267f
Alexander Nurullov <web_soft@mail.ru>
2021-12-29 17:29:24 +0400

    add new task

---
+++ simple/Length of Last Word/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+*/
+
+func main() {
+	fmt.Println(lengthOfLastWord("Hello World"))
+	fmt.Println(lengthOfLastWord("   fly me   to   the moon  "))
+	fmt.Println(lengthOfLastWord("luffy is still joyboy"))
+}
+
+func lengthOfLastWord(s string) int {
+	sl := strings.Split(strings.Trim(s, " "), " ")
+	return len(sl[len(sl)-1])
+}
