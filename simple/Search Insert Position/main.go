9d0d84bd47e89c15b585115cd092d665f3fd4eec
Alexander Nurullov <web_soft@mail.ru>
2022-06-03 19:55:42 +0400

    add new simple task

---
+++ simple/Search Insert Position/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+*/
+
+func main(){
+	fmt.Println(searchInsert([]int{1,3,5,6},5))
+	fmt.Println(searchInsert([]int{1,3,5,6},2))
+	fmt.Println(searchInsert([]int{1,3,5,6},7))
+}
+
+func searchInsert(nums []int, target int) int {
+	return sort.SearchInts(nums, target)
+}
